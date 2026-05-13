f31464534a395442dfef0ef25f175f4a3a2f510b
Prarthana D <prarthanadarbhe.com>
2026-05-13 19:48:07 +0530

    Deployment setup

--- app.py
+++ app.py
@@ -82,6 +82,13 @@ def analyze():
     )
 
 
-# Run Flask server
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )
